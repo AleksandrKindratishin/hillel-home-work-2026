3e27ac524a11be9654a5f6b426e9623b2175b4ad
Aleksandr Kindratishin <kindratishin24kindra@gmail.com>
2026-09-24 23:08:29 +0300

    added 2 task to lesson4 derectory

--- lesson4/index.js
+++ lesson4/index.js
@@ -3,6 +3,34 @@
 
 
 
-let name = prompt("What is your name?");
+// let name = prompt("What is your name?");
+//
+// alert(`Hello ${name}! How are you?`);
 
-alert(`Hello ${name}! How are you?`);
+
+
+
+
+
+
+// Дано тризначне число, яке надае користувач, потрибно визначити:
+//
+//     Чи правда, що всі цифри однакові?
+//     Чи є серед цифр цифри однакові?
+
+let userNumber = +prompt("Enter your number");
+
+if (userNumber >= 100 && userNumber <= 999) {
+    let userNumberToStr = String(userNumber);
+
+    if (userNumberToStr[0] === userNumberToStr[1] && userNumberToStr[1] === userNumberToStr[2]) {
+        alert('All numbers is identical!');
+    } else if (userNumberToStr[0] === userNumberToStr[1] || userNumberToStr[0] === userNumberToStr[2] || userNumberToStr[1] === userNumberToStr[2]) {
+        alert('Some numbers are identical!');
+    } else {
+        alert('All numbers different!')
+    }
+} else {
+    alert('Reload page and enter correct number!');
+}
+
